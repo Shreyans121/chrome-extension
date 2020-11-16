b93b8307d53a12873ecbd420354f1dd2928ae4e2
Shreyans Jain <jainshreyans121@gmail.com>
2020-11-16 18:24:03 +0530

    edited js files

--- js/background.js
+++ js/background.js
@@ -1,3 +1,17 @@
+var activeTabUrl = "";
+var div = document.getElementById("actualTime");
+
+var totalTimeOnWebsites = 0;
+
+var isUserActive = true;
+
+var today;
+
+var todayStorageName;
+var websitesToTrack;
+
+var isFirstRun = false;
+
 var barWidth = 15;
 var barSpacing = 10;
 var timeQuotes = [
@@ -16,6 +30,209 @@ var timeQuotes = [
     "the best time to start was last year. failing that, today will do"
 ];
 
+function getActiveWebsite() {
+    return extractDomain(activeTabUrl);
+}
+
+function isUserActiveNow() {
+    return isUserActive;
+}
+
+chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+        isFirstRun = true;
+        var data = {};
+
+        today = numDaysSinceUTC();
+        todayStorageName = "timeData" + numDaysSinceUTC();
+        totalTimeOnWebsites = 0;
+        websitesToTrack = ["twitter.com", "facebook.com", "plus.google.com"];
+
+        data[todayStorageName] = totalTimeOnWebsites;
+        data["today"] = numDaysSinceUTC();
+        data["trackData"] = JSON.stringify(websitesToTrack);
+        data["sitesLocked"] = false;
+        chrome.storage.local.set(data, function(){});
+    }
+});
+
+chrome.storage.onChanged.addListener(function(){
+    chrome.storage.local.get("trackData", function(result){
+        console.log(result.trackData);
+        websitesToTrack = JSON.parse(result.trackData);
+    });
+});
+
+startUp();
+
+function startUp() {
+    chrome.storage.local.get(null, function(result){
+        console.log(result)
+        totalTimeOnWebsites = result["timeData" + numDaysSinceUTC()];
+        today = result["today"];
+        websitesToTrack = (result.trackData);
+    });
+    updateActiveTabUrl();
+
+    registerEvents(); 
+
+    isUserActive = true;
+
+    updateData();
+    setInterval(function(){
+    if(isNewDay()){
+        updateData();
+      }
+    }, 1000);
+
+}
+
+
+
+function updateData(){
+    // totalTimeOnWebsites = 0;
+    var newStorageName = "timeData" + numDaysSinceUTC();
+    var newData = {};
+    newData[newStorageName] = totalTimeOnWebsites;
+    todayStorageName = newStorageName;
+    today = numDaysSinceUTC();
+    newData["today"] = numDaysSinceUTC();
+    console.log(newData)
+    chrome.storage.local.set(newData, function(){});
+    displayTime();
+}
+function displayTime() {
+    
+        var totalTimeSpent = totalTimeOnWebsites;
+        console.log(totalTimeSpent);
+        var div = document.getElementById("actualTime");
+        div.innerHTML = getReadableTime(totalTimeSpent);
+        var storageName = "timeData" + numDaysSinceUTC();
+        var dataToBeWritten = {};
+        dataToBeWritten[storageName] = totalTimeSpent;
+        dataToBeWritten["today"] = numDaysSinceUTC();
+        // console.log(dataToBeWritten);
+        chrome.storage.local.set(dataToBeWritten, function(){});
+    
+}
+
+function getReadableTime(totalSeconds) {
+    var seconds = totalSeconds % 60;
+    var minutes = (Math.floor(totalSeconds/60))%60;
+    var hours = (Math.floor(totalSeconds/3600));
+    var readableTime = '';
+    if (hours > 1) 
+        readableTime += hours + ' hours ';
+    else if(hours == 1)
+        readableTime += hours + ' hour '
+    if (minutes > 1)
+        readableTime += minutes + ' minutes and ' ;
+    else if(minutes == 1)
+        readableTime += minutes + ' minute and '
+    if(seconds == 1){
+        readableTime += seconds + ' second ';
+    }else{
+        readableTime += seconds + ' seconds ';
+    }
+    return readableTime;
+}
+
+function registerEvents() {
+    chrome.tabs.onActivated.addListener(function(activeInfo) {
+        updateActiveTabUrl();
+    });
+
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+        updateActiveTabUrl();
+    });
+
+    chrome.windows.onFocusChanged.addListener(function(windowId) {
+        if (windowId === chrome.windows.WINDOW_ID_NONE) {
+            isUserActive = false;
+        } else {
+            isUserActive = true;
+        }
+        updateActiveTabUrl();
+    });
+    console.log("reg")
+    window.setInterval(intervalListener, 1000); 
+}
+
+function intervalListener() {
+    updateTotalTime();
+}
+
+function updateTotalTime() {
+    // console.log(totalTimeOnWebsites)
+    var currDomain = getActiveWebsite();
+    console.log(isWatchedWebsite(currDomain))  
+    if(isUserActiveNow() && isWatchedWebsite(currDomain)){
+        totalTimeOnWebsites += 1; 
+    }
+    updateData();
+    // totalTimeOnWebsites += 1;
+}
+
+function isWatchedWebsite(domainName){
+    // console.log(domainName);
+    var idx = websitesToTrack.indexOf(domainName);
+    if(idx == -1) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function getLastRefreshTime() {
+    return lastRefreshTimeStamp;
+}
+
+function updateActiveTabUrl() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) { 
+        console.log(tabs)
+        if (tabs.length < 1) { 
+            activeTabUrl = null;
+        } else {
+            activeTabUrl = tabs[0].url;
+            console.log(activeTabUrl)
+        }
+    });
+}
+
+function extractDomain(str) {
+    var strList = str.split(":\/\/");
+    if (strList.length > 1) {
+        str = strList[1];
+    } else {
+        str = strList[0];
+    }
+    str = str.replace(/www\./g,'');
+    
+    var domainName = str.split('\/')[0];
+    return domainName;
+}
+
+
+function getTimeOnFbTwitter(){
+    return totalTimeOnWebsites;
+}
+
+function numDaysSinceUTC(){
+    var NUM_MILI_IN_A_DAY = 86400000;
+    var today = new Date();
+    var utcMili = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()); // miliseconds since UTC
+    return (utcMili/ NUM_MILI_IN_A_DAY);
+}
+
+function isNewDay(){
+    return (numDaysSinceUTC() - today >= 1);
+}
+
+function todayTimeData(){
+    return todayStorageName;
+}
+
 $(document).ready(function(){
 
     var backgroundPage = chrome.extension.getBackgroundPage();
@@ -24,7 +241,7 @@ $(document).ready(function(){
     }
  
     chrome.storage.local.get("sitesLocked", function(result){
-        result.sitesLocked = false;
+        // result.sitesLocked = false;
         if(backgroundPage !== null && backgroundPage.isFirstRun) {
             $('.settingsPanel').addClass('is-visible');
             $('.tracksiteInput').addClass('inputEnabled');
@@ -121,10 +338,12 @@ $(document).ready(function(){
                 $('.buttonsContainer').append("<img src = \"images/button_LOCK.png\" class = \"lockBtn\"><img src = \"images/button_OK.png\" class = \"done\">");
             });
 
-        }
+        }        
 
         if($(event.target).is('.settingsPanel') && !chrome.extension.getBackgroundPage().isFirstRun) {
+            // chrome.storage.local.set({"sitesLocked" : false}, function(){});
             chrome.storage.local.get("sitesLocked", function(result){
+                console.log(result)
                 if(!result.sitesLocked) {
                     $('.settingsPanel').removeClass('is-visible');
                     $('.editBtn').remove();
@@ -192,112 +411,75 @@ $(document).ready(function(){
         trackerDisplay.innerHTML = "going great!";
     }
 
-    var forLabels = [];
-    var forValues = [];
-    chrome.storage.local.get(null, function(extDatas){
-        // console.log(extDatas)
+    // var forLabels = [];
+    // var forValues = [];
+    // chrome.storage.local.get(null, function(extDatas){
+    //     // console.log(extDatas)
 
-        for(var prop in extDatas){
-            if(prop.lastIndexOf("timeData", 0) === 0){
-                forLabels.push(prop);
-                forValues.push(extDatas[prop]);
-                // console.log(forLabels)
-            }
-        }
+    //     for(var prop in extDatas){
+    //         if(prop.lastIndexOf("timeData", 0) === 0){
+    //             forLabels.push(prop);
+    //             forValues.push(extDatas[prop]);
+    //             console.log(forLabels)
+    //         }
+    //     }
 
 
-        var barChartData = {
-                labels : [].concat(forLabels),
-                datasets : [
-                    {
-                        fillColor : "rgba(220,220,220,0.6)",
-                        data : [].concat(forValues)
-                    }
-                ],
+    //     var barChartData = {
+    //             labels : [].concat(forLabels),
+    //             datasets : [
+    //                 {
+    //                     fillColor : "rgba(220,220,220,0.6)",
+    //                     data : [].concat(forValues)
+    //                 }
+    //             ],
 
-                getNumData : function(){
-                    return Number(this.labels.length);
-                }
-        }
+    //             getNumData : function(){
+    //                 return Number(this.labels.length);
+    //             }
+    //     }
 
-        var chartView = document.getElementById("myChart");
-        var ctx = chartView.getContext("2d");
-        window.myBar = new Chart(ctx).Bar(barChartData, {
-            responsive : false,
-            barShowStroke : false,
-            scaleShowGridLines : false,
-            showScale : false,
-            showTooltips :false,
-            barValueSpacing  : 13
-        });
+    //     var chartView = document.getElementById("myChart");
+    //     var ctx = chartView.getContext("2d");
+    //     window.myBar = new Chart(ctx).Bar(barChartData, {
+    //         responsive : false,
+    //         barShowStroke : false,
+    //         scaleShowGridLines : false,
+    //         showScale : false,
+    //         showTooltips :false,
+    //         barValueSpacing  : 13
+    //     });
 
-        var totalWidthBars = barWidth * barChartData.getNumData();
-        var totalWidthSpacing = barSpacing * (barChartData.getNumData() - 1);
-        var totalWidthChart = totalWidthBars + totalWidthSpacing;
-        var left = ($(window).width() - totalWidthChart)/2;
+    //     var totalWidthBars = barWidth * barChartData.getNumData();
+    //     var totalWidthSpacing = barSpacing * (barChartData.getNumData() - 1);
+    //     var totalWidthChart = totalWidthBars + totalWidthSpacing;
+    //     var left = ($(window).width() - totalWidthChart)/2;
 
-        $("#myChart").css({
-            "width": totalWidthChart,
-            "height" : "50%",
-            "left" : left
-        });
-    });
+    //     $("#myChart").css({
+    //         "width": totalWidthChart,
+    //         "height" : "50%",
+    //         "left" : left
+    //     });
+    // });
 
 
 });
 
-$(window).resize(function(){
-    var totalWidthChart = $("#myChart").width();
-    var left = ($(window).width() - totalWidthChart)/2;
+// $(window).resize(function(){
+//     var totalWidthChart = $("#myChart").width();
+//     var left = ($(window).width() - totalWidthChart)/2;
 
-    var windowHeight = $(window).height();
-    var windowWidth  = $(window).width();
+//     var windowHeight = $(window).height();
+//     var windowWidth  = $(window).width();
 
-    $("#myChart").css({
-        "width": totalWidthChart,
-        "height" :  windowHeight * 0.5,
-        "left" : left
-    });
+//     $("#myChart").css({
+//         "width": totalWidthChart,
+//         "height" :  windowHeight * 0.5,
+//         "left" : left
+//     });
 
-    $(".container").css({
-        "height" : windowHeight * 0.36,
-        "margin-top" : windowHeight * 0.25
-    });
-});
-
-function displayTime() {
-    var backgroundPage = chrome.extension.getBackgroundPage();
-    if(backgroundPage != null){
-        var totalTimeSpent = chrome.extension.getBackgroundPage().totalTimeOnWebsites;
-        console.log(totalTimeSpent);
-        // var div = document.getElementById("actualTime");
-        // div.innerHTML = getReadableTime(totalTimeSpent);
-        var storageName = "timeData" + chrome.extension.getBackgroundPage().numDaysSinceUTC();
-        var dataToBeWritten = {};
-        dataToBeWritten[storageName] = totalTimeSpent;
-        dataToBeWritten["today"] = chrome.extension.getBackgroundPage().numDaysSinceUTC();
-        // console.log(dataToBeWritten);
-        chrome.storage.local.set(dataToBeWritten, function(){});
-    }
-}
-
-function getReadableTime(totalSeconds) {
-    var seconds = totalSeconds % 60;
-    var minutes = (Math.floor(totalSeconds/60))%60;
-    var hours = (Math.floor(totalSeconds/3600));
-    var readableTime = '';
-    if (hours > 1) 
-        readableTime += hours + ' hours ';
-    else if(hours == 1)
-        readableTime += hours + ' hour '
-    if (minutes > 1)
-        readableTime += minutes + ' minutes and ' ;
-    else if(minutes == 1)
-        readableTime += minutes + ' minute and '
-    if(seconds == 1){
-        readableTime += seconds + ' second ';
-    }else{
-        readableTime += seconds + ' seconds ';
-    }
-    return readableTime;
-}
+//     $(".container").css({
+//         "height" : windowHeight * 0.36,
+//         "margin-top" : windowHeight * 0.25
+//     });
+// });
